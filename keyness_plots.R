d0839597b236b1516e5d522825704172105f4fb1
Harry Ganz <harryyehudah.ganz@gartner.com>
2020-03-04 15:28:45 -0500

    Fixes filename for keyness plot

--- keyness_plots.R
+++ keyness_plots.R
@@ -4,7 +4,7 @@ library(ggplot2)
 
 kp1 <- satisfaction_keyness %>%
   ggplot(aes(x = reorder(feature, G2), y = G2)) +
-  geom_bar(stat = "identity", aes(fill = ifelse(G2 > 0, "4-5 Stars", "0-3 Stars"))) +
+  geom_bar(stat = "identity", aes(fill = ifelse(G2 > 0, "4-5 Stars", "1-3 Stars"))) +
   coord_flip() +
   scale_fill_discrete("Rating", h = c(260, 375), c = 360, l = 65) +
   scale_y_continuous("Log-Likelihood") +
@@ -12,5 +12,5 @@ kp1 <- satisfaction_keyness %>%
   ggtitle("Key terms in Amazon reviews of athletic shoes") + 
   theme_default
 
-ggsave("./figures/satsisfaction_keyterms.png", plot = kp1, height = 6, width = 8, units = "in", dpi = "retina")
+ggsave("./figures/satisfaction_keyterms.png", plot = kp1, height = 6, width = 8, units = "in", dpi = "retina")
   
